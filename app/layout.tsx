18baa4ea4ecf37bc9f53c1db319359e8c5c893f5
ugochukwudev <paulambrose5002@gmail.com>
2023-07-03 03:11:20 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Inter } from "next/font/google";
+import { ChakraProvider } from "@chakra-ui/react";
+import WalletContextProvider from "../components/WalletContextProvider";
+import { extendTheme } from "@chakra-ui/react";
+//const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Buildoors",
+  description: "The NFT Collection for Buildoors",
+};
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75)",
+};
+
+const theme = extendTheme({ colors });
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={""}>
+        <ChakraProvider theme={theme}>
+          <WalletContextProvider>{children}</WalletContextProvider>
+        </ChakraProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Box, Center, Spacer, Stack } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import NavBar from "@/components/NavBar";
+import Disconnected from "@/components/Disconnected";
+import { useWallet } from "@solana/wallet-adapter-react";
+import Connected from "../components/Connected";
+const Home: NextPage = () => {
+  const { connected } = useWallet();
+  return (
+    <div className={styles.container}>
+      <Box
+        w="full"
+        bgImage={"url(/home-background.svg)"}
+        backgroundPosition="center"
+      >
+        <Stack w="full" h="" justify="center">
+          <NavBar />
+
+          <Spacer />
+          <Center>
+            {/* If connected, the second view, otherwise the first */}
+            <Disconnected />
+          </Center>
+          <Spacer />
+
+          <Center>{connected ? <Connected /> : <Disconnected />}</Center>
+        </Stack>
+      </Box>
+    </div>
+  );
+};
+
+export default Home;
